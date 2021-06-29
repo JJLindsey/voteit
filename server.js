9ba5a883af534fd30f988215507dc60c2c50a4ff
JJLindsey <jlindsey010@gmail.com>
2021-06-29 15:33:54 -0500

    connect to db and query function

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const { restElement } = require('@babel/types');
+//const { restElement } = require('@babel/types');
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -8,12 +9,28 @@ const app = express()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your MySQL username,
+        user: 'root',
+        //your MySQL password
+        password: '0wXeV88db!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 //GET test route 
 // app.get('/', (req, res) => {
 //     res.json({
 //         message: 'Hello World'
 //     });
 // });
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default repsonse for any other request
 app.use((req, res) => {
