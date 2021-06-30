e00fdd413004acb46aea57e7189d9598183ecf2a
JJLindsey <jlindsey010@gmail.com>
2021-06-30 11:13:56 -0500

    add queries for delete and create

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //const { restElement } = require('@babel/types');
 const mysql = require('mysql2');
 const express = require('express');
+const { conditionalExpression } = require('@babel/types');
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -28,8 +29,36 @@ const db = mysql.createConnection(
 //     });
 // });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+//GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+//DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE  id = ?`, 1, (err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?, ?, ?, ?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 //default repsonse for any other request
